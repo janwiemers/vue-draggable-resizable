973b3380548d7ffa331700b7d5ac18b330af5763
Jan Wiemers <Jan.Wiemers@nielsen.com>
2020-01-10 19:14:13 +0100

    Update dependencies and remove lookup for global vue

--- src/index.js
+++ src/index.js
@@ -3,23 +3,23 @@ import './components/vue-draggable-resizable.css'
 import VueDraggableResizable from './components/vue-draggable-resizable'
 
 export function install (Vue) {
-  if (install.installed) return
-  install.installed = true
-  Vue.component('VueDraggableResizable', VueDraggableResizable)
+  // if (install.installed) return
+  // install.installed = true
+  // Vue.component('VueDraggableResizable', VueDraggableResizable)
 }
 
-const plugin = {
-  install
-}
+// const plugin = {
+//   install
+// }
 
-let GlobalVue = null
-if (typeof window !== 'undefined') {
-  GlobalVue = window.Vue
-} else if (typeof global !== 'undefined') {
-  GlobalVue = global.Vue
-}
-if (GlobalVue) {
-  GlobalVue.use(plugin)
-}
+// let GlobalVue = null
+// if (typeof window !== 'undefined') {
+//   GlobalVue = window.Vue
+// } else if (typeof global !== 'undefined') {
+//   GlobalVue = global.Vue
+// }
+// if (GlobalVue) {
+//   GlobalVue.use(plugin)
+// }
 
 export default VueDraggableResizable
